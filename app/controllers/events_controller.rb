1514113372e15b038aab04d359ddeb34c673ad07
JuliCarracedo <julicarra@hotmail.com>
2021-06-22 19:20:25 -0300

    Add show event views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except:[:index] 
   def index
-    @events = Event.all
+    @events = Event.all.order(:date)
+    @users = User.all
   end
   def show
     @event = Event.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'events#index'
-  devise_for :users
-  get 'events/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  resources :events, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
