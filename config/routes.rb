b2f7b76ce3f4b8f23c3e7d4bbeb6ff6ad49d5f3a
Enio N. de Souza <enionsouza@gmail.com>
2021-06-23 14:41:25 -0400

    Delete Devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'events#index'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   resources :events, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
