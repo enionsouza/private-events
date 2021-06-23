0d56807a0aafda21b6167efabd4910b57c0cfb88
Enio N. de Souza <enionsouza@gmail.com>
2021-06-23 15:23:16 -0400

    Add Show page for Users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,11 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, except:[:index] 
+  before_action :authenticate_user!, except:[:index]
+
   def index
     @events = Event.all.order(:date)
     @users = User.all
   end
+
   def show
     @event = Event.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
   root to: 'events#index'
   devise_for :users
   resources :events, only: [:index, :show]
+  get '/users/:id', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
