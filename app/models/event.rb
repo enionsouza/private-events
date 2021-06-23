70b94153cd6976190803674231f4cd8e3ba2cdfb
JuliCarracedo <julicarra@hotmail.com>
2021-06-23 18:19:25 -0300

    Add split tables for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :creator
   has_many :attendees
+
+  def self.past
+    where('date < ?',Time.now ).order(:date)
+  end
+  def self.upcoming
+    where('date >= ?',Time.now ).order(:date)
+  end
 end
